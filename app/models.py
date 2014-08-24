52f9d2d66bf6e50b5033f764630d7f8976aaf6d6
Fredy Quispe <fredysqm@gmail.com>
2014-08-24 16:16:02 -0500

    avance template

--- app/models.py
+++ app/models.py
@@ -1,17 +1,16 @@
 from django.db import models
 
-
 class universidad(models.Model):
     nombre = models.CharField(max_length=140)
-
+    def __unicode__(self): return "%s" % (self.nombre)
 
 class cargo(models.Model):
     nombre = models.CharField(max_length=60)
-
+    def __unicode__(self): return "%s" % (self.nombre)
 
 class grado(models.Model):
     nombre = models.CharField(max_length=60)
-
+    def __unicode__(self): return "%s" % (self.nombre)
 
 class participante(models.Model):
     dni = models.CharField(max_length=8, unique=True)
@@ -23,12 +22,12 @@ class participante(models.Model):
     email = models.EmailField()
     fijo = models.CharField(max_length=12)
     movil = models.CharField(max_length=12)
-
     universidad = models.ForeignKey(universidad)
     facultad = models.CharField(max_length=140)
     carrera = models.CharField(max_length=140)
     titulo = models.CharField(max_length=140)
     cargo = models.ForeignKey(cargo)
     grado = models.ForeignKey(grado)
-
     investigacion = models.TextField()
+    ingreso = models.DateField(auto_now=True)
+    def __unicode__(self): return "%s" % (self.dni)
--- vrin/urls.py
+++ vrin/urls.py
@@ -1,12 +1,8 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'vrin.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+urlpatterns = patterns('app.views',
+    url(r'^$', 'home_view', name='home_url'),
     url(r'^admin/', include(admin.site.urls)),
 )
