6dabdc8830c8fbbfd622e91d5a53498d12eea633
Fredy Quispe <fredysqm@gmail.com>
2014-08-25 19:54:21 -0500

    navegacion

--- app/views.py
+++ app/views.py
@@ -3,6 +3,10 @@ from django.core.context_processors import csrf
 
 from forms import participante_form
 
+
+def home_view(request):
+    return render(request, 'home.html')
+
 def inscripcion_view(request):
     args = {}
     args.update(csrf(request))
@@ -18,5 +22,5 @@ def inscripcion_view(request):
     args['form'] = form
     return render(request, 'inscripcion.html', args)
 
-def constancia_view(request, id):
-    pass
+def constancia_view(request):
+    return render(request, 'constancia.html')
--- vrin/urls.py
+++ vrin/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('app.views',
+    url(r'^$', 'home_view', name='home_url'),
     url(r'^inscripcion/$', 'inscripcion_view', name='inscripcion_url'),
+    url(r'^constancia/$', 'constancia_view', name='constancia_url'),
     url(r'^admin/', include(admin.site.urls)),
 )
