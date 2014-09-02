0744bb2dbe561205c2c6b328df735f7a638c6181
Fredy Quispe <fredysqm@gmail.com>
2014-09-02 16:10:00 -0500

    mejoras constancia

--- vrin/settings/prod.py
+++ vrin/settings/prod.py
@@ -4,3 +4,36 @@ from .common import *
 DEBUG = False
 TEMPLATE_DEBUG = False
 ALLOWED_HOSTS = ['vrin.roboticaunsaac.club']
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'app',
+    'crispy_forms',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.cache.UpdateCacheMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.cache.FetchFromCacheMiddleware',
+)
+
+CACHE_MIDDLEWARE_ANONYMOUS_ONLY = True
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
+
+SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
